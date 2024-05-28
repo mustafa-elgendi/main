6874c1690ef929fa7580e398157800810858a932
Mustafa elGendi <mustafa.elgendi@gmail.com>
2024-05-28 07:26:03 +0000

    update

--- blackjack/script.js
+++ blackjack/script.js
@@ -1,55 +1,54 @@
-let playerFirstCard = getRandomCard()
-let playerSecondCard = getRandomCard()
-let sum = playerFirstCard + playerSecondCard
-let dealerQue = ""
-let hand = [playerFirstCard, playerSecondCard]
-let inGame = false
+let playerFirstCard = getRandomCard();
+let playerSecondCard = getRandomCard();
+let sum = playerFirstCard + playerSecondCard;
+let dealerQue = "";
+let hand = [playerFirstCard, playerSecondCard];
+let inGame = false;
 
 function getRandomCard() {
-  let RandomCard = Math.floor(Math.random() * 13) + 1
+  let RandomCard = Math.floor(Math.random() * 13) + 1;
   if (RandomCard >= 11) {
-    return 10
+    return 10;
   } else if (RandomCard === 1) {
-    return 11
+    return 11;
   } else {
-    return RandomCard
+    return RandomCard;
   }
 }
 
 function startGame() {
-  inGame = true
-  let playerFirstCard = getRandomCard()
-  let playerSecondCard = getRandomCard()
-  hand = [playerFirstCard, playerSecondCard]
-  sum = playerFirstCard + playerSecondCard
-  checkState()
+  inGame = true;
+  let playerFirstCard = getRandomCard();
+  let playerSecondCard = getRandomCard();
+  hand = [playerFirstCard, playerSecondCard];
+  sum = playerFirstCard + playerSecondCard;
+  checkState();
 }
 
 function checkState() {
   if (sum > 21) {
-    dealerQue = "Game Over!"
-    inGame = false
+    dealerQue = "Game Over!";
+    inGame = false;
   } else if (sum === 21) {
-    dealerQue = "BLACKJACK! YOU WON!"
-    inGame = false
+    dealerQue = "BLACKJACK! YOU WON!";
+    inGame = false;
   } else {
-    dealerQue = "Draw another card?!"
-    inGame = true
+    dealerQue = "Draw another card?!";
+    inGame = true;
   }
-  document.getElementById("sum-el").innerText = (sum)
-  document.getElementById("message-el").innerText = (dealerQue)
-  document.getElementById("hand-el").innerText = (hand)
+  document.getElementById("sum-el").innerText = sum;
+  document.getElementById("message-el").innerText = dealerQue;
+  document.getElementById("hand-el").innerText = hand;
 }
 
 function newCard() {
-  checkState()
+  checkState();
 
   if (inGame === true) {
-    let i = getRandomCard()
-    sum += i
-    hand.push(i)
-    checkState()
+    let i = getRandomCard();
+    sum += i;
+    hand.push(i);
+    checkState();
   } else {
-
   }
 }
