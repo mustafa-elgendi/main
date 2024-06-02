50d7e127e586654251f7ee8e4328dabc2cc3c900
Mustafa elGendi <mustafa.elgendi@gmail.com>
2024-06-02 17:20:15 +0000

    update

--- tax app/script.js
+++ tax app/script.js
@@ -1,22 +1,33 @@
 let vatEl = document.getElementById("vat-el");
 let numInEl = document.getElementById("numIn-el");
+let yEl = document.getElementById("y-el");
+let nEl = document.getElementById("n-el");
 let numOutEl = document.getElementById("numOut-el");
-const numVatBtnEl = document.getElementById("numVatBtn-el");
-const vatNumBtnEl = document.getElementById("vatNumBtn-el");
-
-// let num = 0;
-// let vat = 0;
-
-numVatBtnEl.addEventListener("click", function () {
-  num = numInEl.valueAsNumber;
-  vat = vatEl.valueAsNumber;
-  num += (num * vat) / 100;
-  numOutEl.textContent = num.toFixed(2);
-});
-
-vatNumBtnEl.addEventListener("click", function () {
-  num = numInEl.valueAsNumber;
-  vat = vatEl.valueAsNumber;
-  num = (num / (vat + 100)) * 100;
-  numOutEl.textContent = num.toFixed(2);
+const submitBtnEl = document.getElementById("submitBtn-el");
+submitBtnEl.addEventListener("click", function () {
+  if (numInEl.valueAsNumber === 0 || isNaN(numInEl.valueAsNumber)) {
+    numOutEl.innerHTML = `<p class="error">قم بإدخال المبلغ</p>`;
+  } else if (vatEl.valueAsNumber === 0 || isNaN(vatEl.valueAsNumber)) {
+    numOutEl.innerHTML = `<p class="error">قم بإدخال نسبة الضريبة</p>`;
+  } else {
+    if (document.querySelector('input[name="yn"]:checked').value === "n") {
+      num = numInEl.valueAsNumber;
+      vat = vatEl.valueAsNumber;
+      num += (num * vat) / 100;
+      numOutEl.innerHTML = `
+        <p class="result">المبلغ شامل ضريبة القيمة المضافة </p><span>${num.toFixed(
+          2
+        )}</span>`;
+    } else if (
+      document.querySelector('input[name="yn"]:checked').value === "y"
+    ) {
+      num = numInEl.valueAsNumber;
+      vat = vatEl.valueAsNumber;
+      num = (num / (vat + 100)) * 100;
+      numOutEl.innerHTML = `
+      <p class="result">أصل المبلغ المحسوب عليه الضريبة </p><span>${num.toFixed(
+        2
+      )}</span>`;
+    }
+  }
 });
